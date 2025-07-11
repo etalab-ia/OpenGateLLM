66a011910ded31f368e20af3e7193d3e499ab75b
lvcv4y <lucas04.vayssieres@gmail.com>
2025-07-11 22:55:20 +0200

    fix(merge) schemas and var names

--- app/helpers/models/_modelregistry.py
+++ app/helpers/models/_modelregistry.py
@@ -123,7 +123,7 @@ class ModelRegistry:
             owned_by=owner,
             aliases=aliases,
             routing_strategy=routing_strategy,
-            clients=[model_client],
+            providers=[model_client],
         )
         self._routers[router_id] = router
 
