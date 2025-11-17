c1563eeaa8791f779ee466eed25257d0df78f80d
leoguillaume <leo.ch.guillaume@gmail.com>
2025-11-17 22:19:04 +0100

    optimize eager mode

--- api/utils/qos.py
+++ api/utils/qos.py
@@ -5,10 +5,10 @@ from api.schemas.core.metrics import Metric
 from api.utils.variables import PREFIX__REDIS_METRIC_GAUGE
 
 
-def apply_sync_qos_policy(provider_id: int, qos_metric: Metric, qos_limit: float | None, redis_client: Redis) -> bool:
+def apply_sync_qos_policy(provider_id: int, qos_metric: Metric | None, qos_limit: float | None, redis_client: Redis) -> bool:
     can_be_forwarded = True
 
-    if qos_limit is None:
+    if qos_metric is None or qos_limit is None:
         return can_be_forwarded
 
     if qos_metric == Metric.INFLIGHT:
@@ -21,10 +21,10 @@ def apply_sync_qos_policy(provider_id: int, qos_metric: Metric, qos_limit: float
     return can_be_forwarded
 
 
-async def apply_async_qos_policy(provider_id: int, qos_metric: Metric, qos_limit: float | None, redis_client: AsyncRedis) -> bool:
+async def apply_async_qos_policy(provider_id: int, qos_metric: Metric | None, qos_limit: float | None, redis_client: AsyncRedis) -> bool:
     can_be_forwarded = True
 
-    if qos_limit is None:
+    if qos_metric is None or qos_limit is None:
         return can_be_forwarded
 
     if qos_metric == Metric.INFLIGHT:
