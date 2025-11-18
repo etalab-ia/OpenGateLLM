9dc22f279a5795bbd7c1493f55bd5ae9665fd270
leoguillaume <leo.ch.guillaume@gmail.com>
2025-11-18 21:04:06 +0100

    add missing import from model provider init

--- api/clients/model/__init__.py
+++ api/clients/model/__init__.py
@@ -1,6 +1,7 @@
+from ._albertmodelprovider import AlbertModelProvider
 from ._basemodelprovider import BaseModelProvider
 from ._openaimodelprovider import OpenaiModelProvider
 from ._teimodelprovider import TeiModelProvider
 from ._vllmmodelprovider import VllmModelProvider
 
-__all__ = [BaseModelProvider, OpenaiModelProvider, TeiModelProvider, VllmModelProvider]
+__all__ = [AlbertModelProvider, BaseModelProvider, OpenaiModelProvider, TeiModelProvider, VllmModelProvider]
