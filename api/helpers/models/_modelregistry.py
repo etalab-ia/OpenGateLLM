9c230d0a9b8e00bbd11543c6a040d8a07c6e4460
leoguillaume <leo.ch.guillaume@gmail.com>
2025-12-10 11:04:50 +0100

    wip

--- api/helpers/models/_modelregistry.py
+++ api/helpers/models/_modelregistry.py
@@ -41,6 +41,7 @@ from api.utils.variables import (
     ENDPOINT__CHAT_COMPLETIONS,
     ENDPOINT__EMBEDDINGS,
     ENDPOINT__OCR,
+    ENDPOINT__OCR_BETA,
     ENDPOINT__RERANK,
     PREFIX__CELERY_QUEUE_ROUTING,
 )
@@ -77,12 +78,16 @@ class ModelRegistry:
             ProviderType.ALBERT.value,
             ProviderType.TEI.value,
         ],
+        ModelType.IMAGE_TO_TEXT: [
+            ProviderType.MISTRAL.value,
+        ],
     }
     ENDPOINT_MODEL_TYPE_TABLE = {
         ENDPOINT__AUDIO_TRANSCRIPTIONS: [ModelType.AUTOMATIC_SPEECH_RECOGNITION],
         ENDPOINT__CHAT_COMPLETIONS: [ModelType.TEXT_GENERATION, ModelType.IMAGE_TEXT_TO_TEXT],
         ENDPOINT__EMBEDDINGS: [ModelType.TEXT_EMBEDDINGS_INFERENCE],
-        ENDPOINT__OCR: [ModelType.IMAGE_TEXT_TO_TEXT],
+        ENDPOINT__OCR: [ModelType.IMAGE_TO_TEXT],
+        ENDPOINT__OCR_BETA: [ModelType.IMAGE_TEXT_TO_TEXT],
         ENDPOINT__RERANK: [ModelType.TEXT_CLASSIFICATION],
     }
 
