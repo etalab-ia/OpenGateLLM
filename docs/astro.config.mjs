798563dce2fad87f4d5e776464c664ff4368d8ff
leoguillaume <62661249+leoguillaume@users.noreply.github.com>
2026-03-10 07:10:13 +0000

    chore(doc): update generated documentation and release versions

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -85,7 +85,7 @@ export default defineConfig({
 				},				{
 					label: '[lucide:rocket] Release notes',
 					link: 'https://github.com/etalab-ia/OpenGateLLM/releases',
-					badge: 'v0.4.1',
+					badge: 'v0.4.1post2',
 					attrs: { target: '_blank', style: 'font-size: 0.875rem;' },
 				},
 				{
