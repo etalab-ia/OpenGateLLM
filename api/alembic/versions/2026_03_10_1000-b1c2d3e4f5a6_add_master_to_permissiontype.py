df81afc20e63e93754d94504e3ef1ed8902e915f
tibz <tibo.pendino@epitech.eu>
2026-03-11 18:08:03 +0100

    fix(alembic): fix migration history

--- api/alembic/versions/2026_03_10_1000-b1c2d3e4f5a6_add_master_to_permissiontype.py
+++ api/alembic/versions/2026_03_10_1000-b1c2d3e4f5a6_add_master_to_permissiontype.py
@@ -13,7 +13,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "b1c2d3e4f5a6"
-down_revision: Union[str, None] = "f02a2525b97c"
+down_revision: Union[str, None] = "c206a2bfefe9"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
