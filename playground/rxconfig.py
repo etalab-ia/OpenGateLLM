8e104ef2d350d33e720620bc2702c7491ec6e72d
tibz <72016002+tibo-pdn@users.noreply.github.com>
2026-04-24 16:05:56 +0200

    fix(playground): websocket-error-due-to-reflex-upgrade-in-playground (remove frontend host) (#828)

--- playground/rxconfig.py
+++ playground/rxconfig.py
@@ -12,7 +12,6 @@ config = rx.Config(
     app_name="app",
     plugins=[rx.plugins.SitemapPlugin()],
     backend_port=8500,
-    frontend_port=8501,
     backend_host="0.0.0.0",
     api_url=os.environ.get("REFLEX_BACKEND_URL", "http://localhost:8500"),
     deploy_url=os.environ.get("REFLEX_FRONTEND_URL", "http://localhost:8501"),
