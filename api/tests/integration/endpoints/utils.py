a3819afab38a87cbdcd8ea6671292d44ce5f2690
leoguillaume <leo.ch.guillaume@gmail.com>
2026-05-21 16:00:29 +0200

    fix create provider usecase

--- api/tests/integration/endpoints/utils.py
+++ api/tests/integration/endpoints/utils.py
@@ -4,27 +4,30 @@ import factory
 import httpx
 
 from api.domain.provider.entities import ProviderType
-from api.infrastructure.http.model.albert import AlbertModelHttpClient
-from api.infrastructure.http.model.mistral import MistralModelHttpClient
-from api.infrastructure.http.model.openai import OpenaiModelHttpClient
-from api.infrastructure.http.model.tei import TeiModelHttpClient
-from api.infrastructure.http.model.vllm import VllmModelHttpClient
 
 DEFAULT_PROVIDER_URL = "http://my-test-provider/"
-ENDPOINT_TABLE = {
-    ProviderType.ALBERT: AlbertModelHttpClient.ENDPOINT_TABLE,
-    ProviderType.MISTRAL: MistralModelHttpClient.ENDPOINT_TABLE,
-    ProviderType.OPENAI: OpenaiModelHttpClient.ENDPOINT_TABLE,
-    ProviderType.TEI: TeiModelHttpClient.ENDPOINT_TABLE,
-    ProviderType.VLLM: VllmModelHttpClient.ENDPOINT_TABLE,
+
+MODELS_ENDPOINT_BY_PROVIDER = {
+    ProviderType.ALBERT: "/v1/models",
+    ProviderType.MISTRAL: "/v1/models",
+    ProviderType.OPENAI: "/v1/models",
+    ProviderType.TEI: "/info",
+    ProviderType.VLLM: "/v1/models",
+}
+EMBEDDINGS_ENDPOINT_BY_PROVIDER = {
+    ProviderType.ALBERT: "/v1/embeddings",
+    ProviderType.MISTRAL: "/v1/embeddings",
+    ProviderType.OPENAI: "/v1/embeddings",
+    ProviderType.TEI: "/v1/embeddings",
+    ProviderType.VLLM: "/v1/embeddings",
 }
 
 
 def mock_models_responses(respx_mock, provider_type: ProviderType, body: factory.DictFactory, status_code: int) -> None:
-    url = urljoin(base=DEFAULT_PROVIDER_URL, url=ENDPOINT_TABLE[provider_type].models[1])
+    url = urljoin(DEFAULT_PROVIDER_URL, MODELS_ENDPOINT_BY_PROVIDER[provider_type])
     respx_mock.get(url=url).mock(return_value=httpx.Response(status_code=status_code, json=body))
 
 
 def mock_embeddings_responses(respx_mock, provider_type: ProviderType, body: factory.DictFactory, status_code: int) -> None:
-    url = urljoin(base=DEFAULT_PROVIDER_URL, url=ENDPOINT_TABLE[provider_type].embeddings[1])
+    url = urljoin(DEFAULT_PROVIDER_URL, url=EMBEDDINGS_ENDPOINT_BY_PROVIDER[provider_type])
     respx_mock.post(url=url).mock(return_value=httpx.Response(status_code=status_code, json=body))
--- api/use_cases/admin/providers/_createproviderusecase.py
+++ api/use_cases/admin/providers/_createproviderusecase.py
@@ -60,7 +60,6 @@ class CreateProviderUseCase:
         self.user_with_role_query = user_with_role_query
 
     async def execute(self, command: CreateProviderCommand) -> CreateProviderUseCaseResult:
-
         user_id = command.request_context.get().user_id
         user = await self.user_with_role_query.get_user_with_role_by_id(user_id=user_id)
 
@@ -110,7 +109,7 @@ class CreateProviderUseCase:
 
         result = await self.provider_repository.create_provider(
             router_id=command.router_id,
-            user_id=command.user_id,
+            user_id=user_id,
             provider_type=command.provider_type,
             url=command.url,
             key=command.key,
