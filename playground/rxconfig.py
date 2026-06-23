3176bf0172dd733e87b7e25afd9c5a60149e55fb
leoguillaume <leo.ch.guillaume@gmail.com>
2026-06-23 19:19:05 +0200

    fonctionnel avec et sans sso

--- playground/rxconfig.py
+++ playground/rxconfig.py
@@ -16,6 +16,7 @@ config = rx.Config(
     plugins=[rx.plugins.SitemapPlugin()],
     backend_port=8500,
     backend_host="0.0.0.0",
+    frontend_port=4180,
     api_url=os.environ.get("REFLEX_BACKEND_URL", "http://localhost:8500"),
     deploy_url=os.environ.get("REFLEX_FRONTEND_URL", "http://localhost:8501"),
     frontend_path=os.environ.get("REFLEX_FRONTEND_PATH", ""),
