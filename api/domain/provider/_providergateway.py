c4169a9251c9a7c8612b7df9280bc37086a3932e
Bakr Annour <dinum-304418@MacBook-Pro-de-DINUM-304418.local>
2026-07-22 18:36:00 +0200

    removed ProviderGateway class from provider domain

--- api/domain/provider/_providergateway.py
+++ api/domain/provider/_providergateway.py
@@ -1,27 +1,7 @@
-from abc import ABC, abstractmethod
 from dataclasses import dataclass
-
-from api.domain.model.entities import ModelType as RouterType
-from api.domain.model.errors import ModelNotFoundError
-from api.domain.provider.entities import ProviderType
-from api.domain.provider.errors import ProviderNotReachableError
 
 
 @dataclass
 class ProviderCapabilities:
     max_context_length: int | None
     vector_size: int | None = None
-
-
-class ProviderGateway(ABC):
-    @abstractmethod
-    async def get_capabilities(
-        self,
-        router_type: RouterType,
-        provider_type: ProviderType,
-        url: str,
-        key: str | None,
-        timeout: int,
-        model_name: str,
-    ) -> ProviderCapabilities | ModelNotFoundError | ProviderNotReachableError:
-        pass
