8e9a6187c9f043630470dfe2e7ca1084dc787c9d
leoguillaume <leo.ch.guillaume@gmail.com>
2026-08-24 13:41:55 +0200

    rename legacy endpoint

--- playground/app/features/keys/state.py
+++ playground/app/features/keys/state.py
@@ -115,7 +115,7 @@ class KeysState(EntityState):
         try:
             async with httpx.AsyncClient() as client:
                 response = await client.delete(
-                    url=f"{self.opengatellm_url}/v1/keys/{self.entity_to_delete.id}",
+                    url=f"{self.opengatellm_url}/v1/me/keys/{self.entity_to_delete.id}",
                     headers={"Authorization": f"Bearer {self.api_key}"},
                     timeout=self.opengatellm_timeout,
                 )
